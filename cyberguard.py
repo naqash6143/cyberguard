638d77f9afc8d54270e245ceb02c9504f87e045f
naqash6143 <naqashliaqat9292@gmail.com>
2024-12-30 09:26:11 +0500

    Update cyberguard.py

--- cyberguard.py
+++ cyberguard.py
@@ -36,26 +36,26 @@ from langchain_groq import ChatGroq
 ##############################################################################################################################################################
 st.title("CYBER-GUARD")
 
-loader=PyPDFDirectoryLoader("./knowledgebase_for_chatbot/")
+#loader=PyPDFDirectoryLoader("./knowledgebase_for_chatbot/")
 data = loader.load()
 #split the extracted data into text chunks using the text_splitter, which splits the text based on the specified number of characters and overlap
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-text_chunks = text_splitter.split_documents(data)
+#text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+#text_chunks = text_splitter.split_documents(data)
 #download the embeddings to use to represent text chunks in a vector space, using the pre-trained model "sentence-transformers/all-MiniLM-L6-v2"
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 # create embeddings for each text chunk using the FAISS class, which creates a vector index using FAISS and allows efficient searches between vectors
-vector_store = FAISS.from_documents(text_chunks, embedding=embeddings)
+#vector_store = FAISS.from_documents(text_chunks, embedding=embeddings)
 
-vector_store.save_local("faiss_index")
+#vector_store.save_local("faiss_index")
 
-new_vector_store = FAISS.load_local(
-     "faiss_index", embeddings, allow_dangerous_deserialization=True
- )
+#new_vector_store = FAISS.load_local(
+ #    "faiss_index", embeddings, allow_dangerous_deserialization=True
+ #)
 
 
 
 # # Retrieve and generate using the relevant snippets of the blog.
- retriever = new_vector_store.as_retriever()
+#retriever = new_vector_store.as_retriever()
 
 # from langchain_groq import ChatGroq
 GROQ_API_KEY=os.getenv("GROQ_API_KEY")
@@ -65,21 +65,21 @@ llm = ChatGroq(
      api_key=GROQ_API_KEY
  )
 
- prompt = hub.pull("rlm/rag-prompt")
+ #prompt = hub.pull("rlm/rag-prompt")
 
 
 
 
-def format_docs(docs):
-     return "\n\n".join(doc.page_content for doc in docs)
+#def format_docs(docs):
+#     return "\n\n".join(doc.page_content for doc in docs)
 
 
-rag_chain = (
-     {"context": retriever | format_docs, "question": RunnablePassthrough()}
-     | prompt
-     | llm
-     | StrOutputParser()
- )
+#rag_chain = (
+#     {"context": retriever | format_docs, "question": RunnablePassthrough()}
+#     | prompt
+#     | llm
+#     | StrOutputParser()
+# )
 
 
 
