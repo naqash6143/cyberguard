a9595013c58b47145a5be2cf409b6c87d0f7e7a8
naqash6143 <naqashliaqat9292@gmail.com>
2025-01-03 21:18:11 +0500

    Update cyberguard.py

--- cyberguard.py
+++ cyberguard.py
@@ -218,6 +218,7 @@ if selection=="Dashboard":
         st.write("Improve your Score [Here](https://docs.google.com/forms/d/e/1FAIpQLSd_xz8xAvkVodtLzBqmXyWZBT50-W0VnFllFFyg-DePVuh8ww/viewform)")
 
     st.subheader("Cyber Security Guidelines")
+    st.button(":thumbsup:")
 
     col1,col2,col3=st.columns(3)
     with col1:
