a2ff24b736ef7693e1b7759ca6c7b1da3b057956
naqash6143 <naqashliaqat9292@gmail.com>
2025-01-03 20:58:00 +0500

    Update cyberguard.py

--- cyberguard.py
+++ cyberguard.py
@@ -174,6 +174,7 @@ if selection=="Dashboard":
     # st.write(df.iloc[-1,1:].values)
     responses=df.iloc[-1,1:].values
     st.write(responses)
+    st.write(list(set(correct)))
     correct=[]
     # wrong=[]
     for i,j in zip(responses,advanced_key):
