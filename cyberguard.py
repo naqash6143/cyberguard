588246caab5f06ed4fcafbb700724264761624be
naqash6143 <naqashliaqat9292@gmail.com>
2025-01-03 23:09:23 +0500

    Update cyberguard.py

--- cyberguard.py
+++ cyberguard.py
@@ -30,7 +30,7 @@ import sys
 import os
 import colorama
 from time import sleep 
-st.set_page_config(page_title="CyberGuard", page_icon="🔒", layout="centered", initial_sidebar_state="auto", menu_items=None)
+#st.set_page_config(page_title="CyberGuard", page_icon="🔒", layout="centered", initial_sidebar_state="auto", menu_items=None)
 ##############################################################################################################################################################
 st.title("CYBER-GUARD")
 
