254ff57db987f6ff1db2cc3344b1a0b7945069c6
naqash6143 <naqashliaqat9292@gmail.com>
2025-01-03 19:11:58 +0500

    Update cyberguard.py

--- cyberguard.py
+++ cyberguard.py
@@ -151,7 +151,8 @@ if selection=="Dashboard":
     #https://docs.google.com/spreadsheets/d/14KEJcHSMJf0qiXhSjUJpNdBWORHWUUpgQU-yLtp8xJI/edit?gid=27636577#gid=27636577   
     sheet_name = 'Cyber Quiz (Responses)' # replace with your own sheet name
     sheet_id = '14KEJcHSMJf0qiXhSjUJpNdBWORHWUUpgQU-yLtp8xJI' # replace with your sheet's ID
-
+ 
+#https://docs.google.com/spreadsheets/d/14KEJcHSMJf0qiXhSjUJpNdBWORHWUUpgQU-yLtp8xJI/edit?gid=27636577#gid=27636577
 
 #essential_key=["A combination of uppercase letters, lowercase letters, numbers, and symbols.","Deceptive Content","both types of cybercrime that involve the use of a fake identity to gain access to personal information","a computer or software system that acts as an intermediary between a user and the internet.","sleep problems, attention problems, and feelings of exclusion among teenagers.","97 zero-day vulnerabilities",
  
